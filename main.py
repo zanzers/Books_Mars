692f72611923463e0b53d9dbe7f8dc4f1586a93a
zanzers <nowellsaavedra999@gmail.com>
2023-10-25 20:51:31 +0800

    json file_update

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-import json
+
+# Mark START HERE!!!!!!!!!!!!!!
+
+
+# Mark PART END HERE!!!!!!!!!!!!!!
+
+# MIC PART END HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!
+     
+#   NOWELL PART START HERE!!!!!!!!!!!!
+
+# NOWELL PART END HERE!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# MARK & Marcelot PART START HERE!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+     
+# MARK & Marcelot PART END HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# MARK VEL START HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+     
+# MARK VEL START END HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# DREWW START HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# DREWW START END HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   
