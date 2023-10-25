5a6e8f95a7c9c5ac91926cf06435d521232eaa0a
zanzers <nowellsaavedra999@gmail.com>
2023-10-25 22:43:30 +0800

    file_update

--- main.py
+++ main.py
@@ -2,11 +2,26 @@
 # Mark START HERE!!!!!!!!!!!!!!
 
 
+# Mark START HERE!!!!!!!!!!!!!!
+
 # Mark PART END HERE!!!!!!!!!!!!!!
 
 # MIC PART END HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!
      
-#   NOWELL PART START HERE!!!!!!!!!!!!
+# NOWELL PART START HERE!!!!!!!!!!!!
+
+class Book(Mars_Books):
+     def __init__(self, json_file):
+          super().__init__(json_file)
+
+     def display(self):
+          
+          books = list_book.read_book()
+          for book in books:
+               print("----------------------------")   
+               for key, value in book.items():
+                    print(f"{key}: {value}")  
+                            
 
 # NOWELL PART END HERE!!!!!!!!!!!!!!!!!!!!!!!!!!
 
@@ -24,3 +39,4 @@
 
 # DREWW START END HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    
+
